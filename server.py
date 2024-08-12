906a65b25c4fe5f8a9286847a9f26da7e7239fd5
mngembu <mary.ebob@yahoo.ca>
2024-08-12 13:45:39 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,32 @@
+#first build a simple flask server
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "hello world!"
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+
+#how can we render an HTML webpage crafted inside an HTML file rather than just returning a string like above?
+#create an html file in pycharm
+
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template("index.html")       #use th file path if in a sub folder in the templates folder
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# This renders the html and displays the webpage, however, without any static files(images), unless they are moved to the static folder
+
+
+
+
